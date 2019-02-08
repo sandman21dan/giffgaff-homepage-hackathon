c5ecd64269458bf0e8eefda2e79a9f695ada15f4
Daniel Perez <danielperezvl@gmail.com>
2019-02-08 14:26:16 +0000

    Add lazy loading to main html file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 (function ($) {
-    var oldGetEditor = $.fn.getEditor;
     $.fn.size = function() {
 
-        console.info('test length' + $.length);
         return $.length;
     };
 })(jQuery);
