356e5febbf027790a9365abb887a41514ba7b849
Shajeeve Ananthasivam <shajeeve@mac-shajeeve.local>
2019-02-08 13:05:18 +0000

    jquery-update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+(function ($) {
+    var oldGetEditor = $.fn.getEditor;
+    $.fn.size = function() {
 
+        console.info('test length' + $.length);
+        return $.length;
+    };
+})(jQuery);
